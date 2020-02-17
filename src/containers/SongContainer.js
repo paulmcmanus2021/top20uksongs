82fe080a73bcc69503405d5f9f22af94067c7b7e
Paul McManus <pmcmanus2021@gmail.com>
2020-02-17 20:49:33 +0000

    Started again

--- src/containers/SongContainer.js
+++ src/containers/SongContainer.js
@@ -1,33 +1,34 @@
-import React, { Component } from 'react';
-import SongList from '../components/SongList.js';
-import SongItem from '../components/SongItem.js';
+import React from 'react';
 
-class SongContainer extends Component {
+class SongContainer extends React.Component {
   constructor(props){
     super(props);
-    this.state = {
-      songs: [],
-      selectedSongTitle: ""
-    };
+      this.state = {
+        songs: [],
+        selectedSongTitle: {}
+
+    }
   }
 
   componentDidMount(){
     const url = "https://itunes.apple.com/gb/rss/topsongs/limit=20/json"
-
     fetch(url)
     .then(res => res.json())
-    .then(data => this.setState({songs: data}))
+    .then(data => this.setState({songs: data.feed.entry}))
     .catch(err => console.error);
   }
 
   render(){
+    const selectedSong = this.state.songs.find(song => {
+      return song.title === this.state.selectedSongTitle
+    })
 
-    return (
+    return(
       <div>
-        <h2>Songs</h2>
+        <h1>UK Top 20</h1>
       </div>
     )
   }
-
 }
+
 export default SongContainer;
